5f8b28269f36872fbdc058c8f270f0534ca83d8d
Neil <neilrowland56@yahoo.com>
2022-12-24 10:19:16 +0000

    first commit nextjs tailwind home page layout

---
+++ app/HomeComponents/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="z-10 bg-stone-100 py-4 text-center text-blue-700">
+      <div className="center">
+        <p className="text-sm uppercase">NextJS 13 Demo</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,16 @@
-import './globals.css'
+import "./globals.css";
+import Header from "./HomeComponents/header";
+import Footer from "./HomeComponents/footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
