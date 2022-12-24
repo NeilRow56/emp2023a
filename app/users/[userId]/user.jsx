afdd376c0895c1748f84acedae0d76444478ff31
Neil <neilrowland56@yahoo.com>
2022-12-24 14:53:08 +0000

    individual user page and nested layout

---
+++ app/users/[userId]/user.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const User = ({ user }) => {
+  return (
+    <section className="m-4 py-4">
+      <div className="center">
+        <div className="relative h-40 w-40 rounded-full">
+          <Image
+            src={user?.imageUrl}
+            alt={user?.name}
+            style={{ objectFit: "cover" }}
+            width={150}
+            height={150}
+            priority
+          />
+        </div>
+        <h1 className="text-xl font-bold">{user?.name}</h1>
+        <p className="text-sm text-stone-400">{user?.email}</p>
+      </div>
+    </section>
+  );
+};
+
+export default User;
