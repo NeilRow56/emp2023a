40febcabf3e4744e67370923674a63c9ea4d1133
Neil <neilrowland56@yahoo.com>
2022-12-24 15:40:54 +0000

    Added Loading... to Users

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -3,7 +3,12 @@ const UsersPage = () => {
     <section>
       <div className="px-4 py-4 sm:px-6 lg:px-8">
         <h1 className="mb-3 text-2xl font-semibold tracking-tight">Users</h1>
-        <p>Select a User</p>
+        <div className="flex flex-row  w-[150px] items-center">
+          <div role="img" aria-label="left pointing hand" className=" pb-1 ">
+            👈🏻
+          </div>
+          <h1 className="px-2">Select a User</h1>
+        </div>
       </div>
     </section>
   );
